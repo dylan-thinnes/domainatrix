0508caa61bc057fe8886eb9e4648086234ac54d5
dylan-thinnes <dylan.j.thinnes@gmail.com>
2017-10-05 15:18:10 +0100

    Add comprehensive UI and net communication
    Comprehensive UI means that users should be able to more easily
    understand the concept of the project and know what kinds of options are
    available to them.
    
    Net communication has linked the two halves of the brain. The core
    functionality is now complete and properly operating from server to
    client.
    
    This also resolved some old bugs and generally cleaned the code up a tad
    bit.

--- index.js
+++ index.js
@@ -8,41 +8,58 @@ var DomainData = function (path, initCallback) {
 	this.path = path;
 	this.initCallback = initCallback;
 	this.newWrite = false;
+	this.fileSet = false;
 	if (typeof this.path === "string") {
 		fs.readFile(this.path, "utf8", (function (err, res) {
 			if (err) {
-				//console.log("Creating new file for IdDictionary.");
 				this.newWrite = true;
+				this.initCandidates = 0;
 			} else {
-				//console.log(res, res.split("\n"));
-				this.domains = this.domains.concat(res.split("\n"));
-				this.domains.pop();
+				var candidates = res.split("\n");
+				this.initCandidates = candidates.length + 1;
+				for (var ii = 0; ii < candidates.length; ii++) {
+					var candidateIndex = candidates.indexOf(candidates[ii]);
+					if (candidateIndex !== -1 && candidateIndex < ii) {
+						this.stateControl("initCandidate");
+					} else this.addDomainCandidate(candidates[ii], this.stateControl.bind(this, "initCandidate"));
+				}
+				this.stateControl("initCandidate");
 			}
 			fs.open(this.path, "a", this.setFile.bind(this));
 		}).bind(this));
 	} else {
-		this.initCallback();
+		this.stateControl("file");
 	}
+}
+DomainData.prototype.stateControl = function (event) {
+	if (event === "file") this.fileSet = true;
+	if (event === "initCandidate") this.initCandidates--;
+
+	if (this.fileSet && this.initCandidates === 0) this.initCallback();
 }
 DomainData.prototype.setFile = function (err, fd) {
 	this.file = fd;
-	this.initCallback();
+	this.stateControl("file");
 }
-DomainData.prototype.addDomainCandidate = function (domain, submitter, callback) {
+DomainData.prototype.addDomainCandidate = function (domain, callback) {
+	console.log(this.domains, domain);
 	var parsedDomain = domain.match(/([^\s]+\.|)ed\.ac\.uk$/g);
-	if (domain !== null && this.domains.indexOf(domain) === -1) {
-		var subDomain = parsedDomain[1];
-		dns.lookup(domain, this.lookupDomain.bind(this, domain, callback));
-	} else callback(JSON.stringify({"accepted": "true", "domain": domain}));
+	if (parsedDomain !== null) {
+		if (this.domains.indexOf(domain) !== -1) callback(JSON.stringify({"state": 1}));
+		else {
+			var subDomain = parsedDomain[1];
+			dns.lookup(domain, this.lookupDomain.bind(this, domain, callback));
+		}
+	} else callback(JSON.stringify({"state": 3}));
 }
-DomainData.prototype.lookupDomainCallback = function (domain, callback, err, ip) {
-	if (err) callback(JSON.stringify({"accepted": "true", "domain": domain}));
+DomainData.prototype.lookupDomain = function (domain, callback, err, ip) {
+	if (err) callback(JSON.stringify({"state": 2}));
 	else this.addDomain(domain, callback);
 }
 DomainData.prototype.addDomain = function (domain, callback) {
 	this.domains.push(domain);
-	this.writeDomain();
-	callback(JSON.stringify({"accepted": "true", "domain": domain}));
+	this.writeDomain(domain);
+	callback(JSON.stringify({"state": 0, "domain": domain}));
 }
 DomainData.prototype.writeDomain = function (domain) {
 	if (this.file !== undefined) fs.write(this.file, (this.newWrite ? "" : "\n") + domain, () => {});
@@ -57,13 +74,13 @@ var domData = new DomainData("domains.txt", function () {
 		if (parsedUrl.pathname === "/" || parsedUrl.pathname === "/index.html") {
 			res.end(fs.readFileSync("index.html"));
 		} else if (parsedUrl.pathname === "/add" || parsedUrl.pathname === "/add/index.html") {
-			domData.addDomain(parsedUrl.query.domain, parsedUrl.query.submitter, res.end.bind(res));
+			domData.addDomainCandidate(parsedUrl.query.domain, res.end.bind(res));
 		} else if (parsedUrl.pathname === "/data" || parsedUrl.pathname === "/data/index.html") {
-		
+			res.end(domData.getDomains());
 		} else {
 			res.end();
 		}
 	});
 	server.listen(8080);
-	//console.log(this.getDomains());
+	console.log(this.getDomains());
 });
