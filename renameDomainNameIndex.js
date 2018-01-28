f29f85d0cb22557237fc22ba2cd1bdfa5d931c03
dylan-thinnes <dylan.j.thinnes@gmail.com>
2018-01-28 02:05:03 +0000

    Fix renamer for production

--- renameDomainNameIndex.js
+++ renameDomainNameIndex.js
@@ -1,7 +1,7 @@
 var abstractdb = require("abstractdb");
 var Database = abstractdb("better-sqlite3");
 var db = new Database("app.db");
-/*db.sRun("ALTER TABLE domains RENAME TO oldDomains");
+db.sRun("ALTER TABLE domains RENAME TO oldDomains");
 db.sRun(`CREATE TABLE domains (
     name TEXT PRIMARY KEY,
     dns INTEGER NOT NULL,
@@ -11,7 +11,7 @@ db.sRun(`CREATE TABLE domains (
     http INTEGER NOT NULL,
     httpLastUpdate INTEGER NOT NULL
 )`);
-db.sRun("INSERT INTO domains(name, dns, dnsLastUpdate, ping, pingLastUpdate, http, httpLastUpdate) SELECT name, dns, dnsLastCheck, ping, pingLastCheck, http, httpLastCheck FROM oldDomains");
-db.sRun("DROP TABLE oldDomains");*/
+db.sRun("INSERT INTO domains(name, dns, dnsLastUpdate, ping, pingLastUpdate, http, httpLastUpdate) SELECT domainName, dns, dnsLastCheck, ping, pingLastCheck, http, httpLastCheck FROM oldDomains");
+db.sRun("DROP TABLE oldDomains");
 res = db.sAll("SELECT * FROM domains");
 console.log(res);
