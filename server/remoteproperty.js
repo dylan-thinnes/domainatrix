4e173deeb512f79dfc7329b16649497d379955bc
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-01-30 21:42:11 +0000

    Make subsequent calls to update wait for first one

--- server/remoteproperty.js
+++ server/remoteproperty.js
@@ -6,6 +6,7 @@ var RemoteProperty = function (onChangeCallback, remoteGetter) {
     this.state = RemoteProperty.NEVER_STARTED;
     this.value = undefined;
     this.lastUpdate = 0;
+    this.waiters = [];
 }
 exports = module.exports = RemoteProperty;
 
@@ -35,6 +36,11 @@ RemoteProperty.prototype.get = function () {
     return this.value;
 }
 RemoteProperty.prototype.update = async function () { // Returns boolean dependent on whether an actual update check was made.
+    if (this.state === RemoteProperty.IN_PROGRESS) { // If an update in progress, wait for it to be over then return that value.
+        await this.waitForUpdate();
+        return this.get();
+    }
+
     var currTime = Date.now();
     if (Date.now() < this.lastUpdate + RemoteProperty.UPDATE_THRESHOLD) return false; // Abort if last update was too recent.
     this.lastUpdate = currTime;
@@ -42,5 +48,11 @@ RemoteProperty.prototype.update = async function () { // Returns boolean depende
     this.state = RemoteProperty.IN_PROGRESS;
     var newValue = await new Promise(this.remoteGetter);
     await this.set(newValue);
+    while (this.waiters.length !== 0) (this.waiters.pop())(); // Run resolve for all other waiting update() calls
     return true;
+}
+RemoteProperty.prototype.waitForUpdate = function () {
+    return new Promise((function (resolve, reject) {
+        this.waiters.push(resolve);
+    }).bind(this));
 }
